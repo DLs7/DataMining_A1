8d5dce9dd02ebc3d4ba7a4578d6787240178237b
DLs7 <agasta@inf.ufsm.br>
2021-12-06 21:29:01 -0300

    parse csv

--- main.py
+++ main.py
@@ -1,7 +1,17 @@
+import warnings
+warnings.simplefilter(action='ignore', category=FutureWarning)
+
 import pandas
+import numpy
 
 def main():
-    csv = pandas.read_csv('_ASSOC_PadelStars.csv')
+    csv = pandas.read_csv('_ASSOC_PadelStars.csv').iloc[:, :2].apply(lambda s: s.astype(str).str.lower())
+    csv = csv.apply(lambda s: s.astype(str).str.replace(' ', ''))
+    csv = csv.apply(lambda s: s.astype(str).str.replace('.', ''))
+    csv = csv.apply(lambda s: s.astype(str).str.replace('*', ''))
+    csv = csv.apply(lambda s: s.astype(str).str.replace('juam', 'juan'))
+    csv = csv.apply(lambda s: s.astype(str).str.replace('lúcia', 'lucia'))
+
     print(csv)
 
 if __name__ == "__main__":
