715a709cd97dc6f51245d38f1e4feac35456095f
DLs7 <agasta@inf.ufsm.br>
2021-12-07 21:48:41 -0300

    final

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ import warnings
 warnings.simplefilter(action='ignore', category=FutureWarning)
 
 import numpy, pandas
+from mlxtend.frequent_patterns import association_rules, apriori
+from itertools import chain
 
 def main():
     # Fazemos a leitura das 3 primeiras colunas do CSV, que tem a informação que a gente quer
@@ -15,17 +17,40 @@ def main():
     csv = csv.apply(lambda s: s.astype(str).str.replace('juam', 'juan'))
     csv = csv.apply(lambda s: s.astype(str).str.replace('lúcia', 'lucia'))
 
+    # Parsamos os resultados para True e False e colocamos os times em tuplas
+    d = {'ganhou': True, 'perdeu': False}
+    csv["Resultado"] = csv["Resultado"].map(d)
+    csv["Jogadore(a)s"] = csv["Jogadore(a)s"].apply(lambda s: tuple(s.split(',')))
+
+    # Pegamos nomes individuais da lista e transformamos eles em colunas na matriz binária
+    unique_names = csv["Jogadore(a)s"].unique().tolist()
+    names = set(chain(*unique_names))
+    
+    # Criamos a matriz binária
+    binary_matrix = pandas.DataFrame()
+    for name in names:
+        binary_matrix[name] = csv["Jogadore(a)s"].apply(lambda x: name in x)
+    binary_matrix["Resultado"] = csv["Resultado"]
+
+    # Aplicamos apriori e regras de associação
+    frequent_itemsets = apriori(binary_matrix, min_support=0.01, use_colnames=True)
+    rules = association_rules(frequent_itemsets, metric="support", min_threshold=0.05)
+    rules = rules[rules['consequents'] == {'Resultado'}]
+
+    # Imprimimos a tabela de regras de associação
+    print("Regras de associação: ")
+    print(rules)
+    print("\n")
+
     # Criamos um CSV de equipes a partir do CSV lido.
     # Agrupamos todos os jogos por equipe
     teamStats = csv.groupby("Jogadore(a)s")["Resultado"].agg(list).reset_index(name="Resultados")
-    # Transformamos a equipe em um array
-    teamStats["Jogadore(a)s"] = teamStats["Jogadore(a)s"].apply(lambda s: s.split(","))
     # Contamos quantos jogos a equipe jogou a partir do tamanho do array de resultados gerado
     teamStats["Jogos"] = teamStats["Resultados"].str.len()
     # Contamos as vitórias desta equipe pela quantidade de "ganhou" no array
-    teamStats["Vitórias"] = teamStats["Resultados"].apply(lambda x: x.count("ganhou"))
+    teamStats["Vitórias"] = teamStats["Resultados"].apply(lambda x: x.count(1))
     # Contamos as derrotas desta equipe pela quantidade de "perdeu" no array
-    teamStats["Derrotas"] = teamStats["Resultados"].apply(lambda x: x.count("perdeu"))
+    teamStats["Derrotas"] = teamStats["Resultados"].apply(lambda x: x.count(0))
     # Contamos o win ratio desta equipe dividindo o número de vitórias pelas partidas totais jogadas
     teamStats["Win ratio"] = round(teamStats["Vitórias"] / teamStats["Jogos"], 2)
 
@@ -55,6 +80,7 @@ def main():
 
     print("\nMelhor jogador individual por win ratio: ")
     print(individualStats.sort_values("Win ratio", ascending=False))
+    print("\n")
 
 if __name__ == "__main__":
     main()
